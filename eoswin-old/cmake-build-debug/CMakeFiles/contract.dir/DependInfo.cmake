0d9f34188d7c32f47aeb148ab242a239b5dce9fd
gavin <gavinwyf@163.com>
2018-12-13 11:25:22 +0800

    init

---
+++ eoswin-old/cmake-build-debug/CMakeFiles/contract.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/wyf.wang/Documents/workspace/@EOS/contract/blackjack/blackjack.cpp" "/Users/wyf.wang/Documents/workspace/@EOS/contract/cmake-build-debug/CMakeFiles/contract.dir/blackjack/blackjack.cpp.o"
+  "/Users/wyf.wang/Documents/workspace/@EOS/contract/dice/dice.cpp" "/Users/wyf.wang/Documents/workspace/@EOS/contract/cmake-build-debug/CMakeFiles/contract.dir/dice/dice.cpp.o"
+  "/Users/wyf.wang/Documents/workspace/@EOS/contract/jackpot/jackpot.cpp" "/Users/wyf.wang/Documents/workspace/@EOS/contract/cmake-build-debug/CMakeFiles/contract.dir/jackpot/jackpot.cpp.o"
+  "/Users/wyf.wang/Documents/workspace/@EOS/contract/lottery/lottery.cpp" "/Users/wyf.wang/Documents/workspace/@EOS/contract/cmake-build-debug/CMakeFiles/contract.dir/lottery/lottery.cpp.o"
+  "/Users/wyf.wang/Documents/workspace/@EOS/contract/lottery/token.cpp" "/Users/wyf.wang/Documents/workspace/@EOS/contract/cmake-build-debug/CMakeFiles/contract.dir/lottery/token.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "../blackjack"
+  "../dice"
+  "../jackpot"
+  "../lottery"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ eoswin-old/cmake-build-debug/CMakeFiles/contract.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/contract.dir/blackjack/blackjack.cpp.o"
+  "CMakeFiles/contract.dir/dice/dice.cpp.o"
+  "CMakeFiles/contract.dir/jackpot/jackpot.cpp.o"
+  "CMakeFiles/contract.dir/lottery/lottery.cpp.o"
+  "CMakeFiles/contract.dir/lottery/token.cpp.o"
+  "contract.pdb"
+  "contract"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/contract.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
